5911dcb160f5f47a6964042e51c0de0ef79528f2
Andrew Molis <andrewmolis@gmail.com>
2020-03-24 13:16:05 -0700

    Added subtract functionality

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,5 +1,6 @@
 import unittest
 from mathFunctions.addition import addition
+from mathFunctions.addition import subtract
 
 
 class TestMath(unittest.TestCase):
@@ -9,6 +10,10 @@ class TestMath(unittest.TestCase):
     def test_addition(self):
         sum_nums = addition(3, 1)
         self.assertEqual(4, sum_nums)
+
+    def test_subtract(self):
+        subtract_result = subtract(3, 1)
+        self.assertEqual(2, subtract_result)
 
     def tearDown(self):
         pass
